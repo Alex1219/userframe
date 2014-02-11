2115331c76ee5a85d801d9e535aa671e2d192256
undido <firekid109@hotmail.com>
2014-02-11 04:52:49 -0400

    added examples to show how to use current code

---
+++ examples/logintest.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once("/../inc/login.class.php");
+require_once("/../inc/functions.php");
+require_once("/../inc/config.php");
+
+
+
+$login = new login("unDiDo", encrypt("test"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ examples/registertest.php
@@ -0,0 +1,25 @@
+<?php
+
+
+
+
+require_once("/../inc/login.class.php");
+require_once("/../inc/functions.php");
+require_once("/../inc/config.php");
+
+
+
+
+
+$login = new register("unDiDo", encrypt("test"));
+
+
+
+
+
+
+
+
+
+
+?>
